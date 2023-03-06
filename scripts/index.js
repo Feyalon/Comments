120c91a56996e925066176110a6262c2ba5ab7e9
VonJuel <112874585+VonJuel@users.noreply.github.com>
2023-03-06 20:36:24 +0500

    Updated index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,58 @@
 let comments = [];
 
-document.getElementById('add_comment').onclick = function(){
+function add_content(){
   event.preventDefault();
   let commentName = document.getElementById('name-comment');
   let commentBody = document.getElementById('body-comment');
+  if(commentName.value == "" || commentBody.value == ""){
+    alert("Одно из поля пустое")
+    return false;
+  }
   let comment = {
     name : commentName.value,
     body : commentBody.value,
     time : Date.now()/1000
   }
-  console.log(comment)
+
+
+
+  commentName.value = "";
+  commentBody.value = "";
+  
+  comments.push(comment);
+  showComments();
+}
+
+function saveComments(){
+
+}
+
+function showComments(){
+  let commentStore = document.getElementById('show_comments')
+  let out = ''
+  comments.forEach((el) => {
+    out += `
+      <div>
+        <p>
+          
+        </p>
+        <p>
+          ${el.name}:
+        </p>
+        <p>
+          ${el.body}
+        </p>
+      </div>
+    `;
+
+
+
+
+  })
+  commentStore.innerHTML = out;
+}
+
+
+function timeConverter(){
+  
 }
